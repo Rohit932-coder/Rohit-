4baa113473e6b4a2acea137d6a7ecca05cfde9b7
Rohit932-coder <150343507+Rohit932-coder@users.noreply.github.com>
2024-06-06 19:03:00 +0530

    Add files via upload

---
+++ op_pr_03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    char a;
+    printf("enter char a :");
+    scanf("%c",&a);
+    printf("the char is %c",a);
+    scanf("%d",&a);
+    printf("the char(num) is %d",a);
+
+}
